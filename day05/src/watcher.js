e621e3c9580ce6479c56d3813632838c680a23db
lay <1581133593@qq.com>
2021-07-14 10:04:22 +0800

    7/14

--- day05/src/watcher.js
+++ day05/src/watcher.js
@@ -1,4 +1,6 @@
 
+let watcherid = 0;
+
 /** Watcher 观察者, 用于 发射更新的行为 */
 class Watcher {
 
@@ -7,10 +9,11 @@ class Watcher {
    * @param {Object} vm JGVue 实例
    * @param {String|Function} expOrfn 如果是渲染 watcher, 传入的就是渲染函数, 如果是 计算 watcher 传入的就是路径表达式, 暂时只考虑 expOrFn 为函数的情况.
    */
-  constructor(vm, expOrfn) {
+  constructor( vm, expOrfn ) {
     this.vm = vm;
     this.getter = expOrfn;
 
+    this.id = watcherid++;
 
     this.deps = []; // 依赖项
     this.depIds = {}; // 是一个 Set 类型, 用于保证 依赖项的唯一性 ( 简化的代码暂时不实现这一块 )
@@ -21,7 +24,11 @@ class Watcher {
 
   /** 计算, 触发 getter */
   get() {
-    this.getter.call(this.vm, this.vm); // 上下文的问题就解决了
+    pushTarget( this );
+
+    this.getter.call( this.vm, this.vm ); // 上下文的问题就解决了
+
+    popTarget();
   }
 
   /**
@@ -29,17 +36,22 @@ class Watcher {
    * 我们现在只考虑 异步执行 ( 简化的是 同步执行 )
    */
   run() {
-    this.get();
+    this.get(); 
     // 在真正的 vue 中是调用 queueWatcher, 来触发 nextTick 进行异步的执行
   }
 
   /** 对外公开的函数, 用于在 属性发生变化时触发的接口 */
   update() {
-    this.run();
+    this.run(); 
   }
 
   /** 清空依赖队列 */
   cleanupDep() {
 
+  }
+
+  /** 将 当前的 dep 与 当前的 watcher 关联 */
+  addDep( dep ) {
+    this.deps.push( dep );
   }
 }
